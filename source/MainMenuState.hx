6d630cbb185e5ccc6ea0e40277429a47520e1cd5
TwistChris <95858593+TwistChris@users.noreply.github.com>
2021-12-20 15:04:28 +0100

    Add files via upload

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -228,11 +228,11 @@ class MainMenuState extends MusicBeatState
 							FlxFlicker.flicker(spr, 1, 0.06, false, false, function(flick:FlxFlicker)
 							{
 							        var daChoice:String = optionShit[curSelected];
-                                                   #if charselection
+                                                                   #if charselection
 							        if(spr.ID == optionShit.length){
-							 LoadingState.loadAndSwitchState(new CharacterSelection());
-                                                   }
-                                                   #end
+							                          LoadingState.loadAndSwitchState(new CharacterSelection());
+                                                                   }
+                                                                   #end
 								switch (daChoice)
 								{
 									case 'story_mode':
