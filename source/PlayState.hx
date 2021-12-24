4caccd8d1b0915a409ef31b1b3b24dca97ff2979
TwistChris <95858593+TwistChris@users.noreply.github.com>
2021-12-24 15:45:00 +0100

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -658,7 +658,7 @@ class PlayState extends MusicBeatState
                 tankman = new Character(-100, 100, tankman);
 
              
-                if(PlayStateChangleables.Optimize);
+                if(PlayStateChangleables.Optimize)
                 {
 
 		add(gfGroup);
