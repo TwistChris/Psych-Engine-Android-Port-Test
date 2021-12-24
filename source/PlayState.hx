636c22cd22c8d527f8083150b336b48b97953829
TwistChris <95858593+TwistChris@users.noreply.github.com>
2021-12-24 18:01:50 +0100

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -126,6 +126,8 @@ class PlayState extends MusicBeatState
 	public static var storyPlaylist:Array<String> = [];
 	public static var storyDifficulty:Int = 1;
 
+        public static var bfsel:Int = 0;
+
 	public var vocals:FlxSound;
 
 	public var dad:Character;
@@ -728,10 +730,6 @@ class PlayState extends MusicBeatState
 		dad = new Character(0, 0, SONG.player2);
 		startCharacterPos(dad, true);
 		dadGroup.add(dad);
-
-		boyfriend = new Boyfriend(0, 0, SONG.player1);
-		startCharacterPos(boyfriend);
-		boyfriendGroup.add(boyfriend);
 		
 		var camPos:FlxPoint = new FlxPoint(gf.getGraphicMidpoint().x, gf.getGraphicMidpoint().y);
 		camPos.x += gf.cameraPosition[0];
@@ -740,6 +738,29 @@ class PlayState extends MusicBeatState
 		if(dad.curCharacter.startsWith('gf')) {
 			dad.setPosition(GF_X, GF_Y);
 			gf.visible = false;
+	        }
+
+		switch bfsel{
+			case 0:
+				boyfriend = new Boyfriend(770, 450, SONG.player1);
+                                startCharacterPos(boyfriend);
+		                boyfriendGroup.add(boyfriend);
+				trace("beta!");
+			case 1:
+				boyfriend = new Boyfriend(770, 450, SONG.player1 + '-blue');
+                                startCharacterPos(boyfriend);
+		                boyfriendGroup.add(boyfriend);
+				trace("blue!");
+			case 2:
+				boyfriend = new Boyfriend(770, 450, SONG.player1 + '-mean');
+                                startCharacterPos(boyfriend);
+		                boyfriendGroup.add(boyfriend);
+				trace("mean!");
+			default:
+				trace("default!");
+				boyfriend = new Boyfriend(770, 450, SONG.player1);
+                                startCharacterPos(boyfriend);
+		                boyfriendGroup.add(boyfriend);
 		}
 
 		switch(curStage)
