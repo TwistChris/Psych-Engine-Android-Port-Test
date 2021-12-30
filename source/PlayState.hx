5d1c23bc1afbea327717a7c1617a05f3ad66f73f
TwistChris <95858593+TwistChris@users.noreply.github.com>
2021-12-30 03:02:50 +0100

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1622,6 +1622,13 @@ class PlayState extends MusicBeatState
 							swagNote.noteType = 'GF Sing';
 							trace("got gf notes");
 						}
+
+                                        if (section.momSection){
+							trace("got mom section");
+						if (songNotes[5] == null || songNotes[5] == ''|| songNotes[5].length ==0){
+							swagNote.noteType = 'MOM Sing';
+							trace("got mom notes");
+						}
 					}
 					
 					swagNote.scrollFactor.set();
