8cadff41f387075554fb02596d5c4fdd96f20478
TwistChris <95858593+TwistChris@users.noreply.github.com>
2021-12-30 19:58:00 +0100

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1624,8 +1624,10 @@ class PlayState extends MusicBeatState
 
                                         swagNote.noteType = songNotes[5];
                                         if(!Std.isOfType(songNotes[5], String)) swagNote.noteType = editors.ChartingState.noteTypeList[songNotes[5]]; //Backward compatibility + compatibility with Week 7 charts
-                                             
 
+
+                                              if (section.momSection){
+							trace("got mom section");
 					         if (songNotes[5] == null || songNotes[5] == ''|| songNotes[5].length ==0){
 							swagNote.noteType = 'MOM Sing';
 							trace("got mom notes");
